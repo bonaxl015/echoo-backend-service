aea3b338f31ff2791a199e7db5880b9f989de515
Bon Axl Feeser <78939695+bonaxl015@users.noreply.github.com>
2025-09-13 12:36:44 +0800

    feat(prisma): database setup and prisma migrations (#1)

---
+++ prisma/migrations/20250913042721_user_migration/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" CHAR(100) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "profilePhoto" VARCHAR(255),
+    "bio" VARCHAR(255),
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
--- src/config/env.ts
+++ src/config/env.ts
@@ -3,5 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const ENV = {
-	PORT: process.env.PORT || 5051
+	PORT: process.env.PORT || 5051,
+	DATABASE_URL: process.env.DATABASE_URL || ''
 };
