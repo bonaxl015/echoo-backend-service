9a205c28d0b1e782501f6d91a829132f2c55978d
bonaxl015 <bonaxl015@gmail.com>
2025-09-17 21:23:12 +0800

    feat(cloudinary): cloudinary configuration

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,21 +1,20 @@
 import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
-import { v4 as uuidv4 } from 'uuid';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import cloudinary from '../config/cloudinary';
+import { v4 as uuidV4 } from 'uuid';
 import { ONE_MEGABYTE } from '../constants';
 
-// Make sure upload folder exists
-const uploadPath = path.join(__dirname, '../../uploads');
-if (!fs.existsSync(uploadPath)) {
-	fs.mkdirSync(uploadPath);
-}
+const storage = new CloudinaryStorage({
+	cloudinary,
+	params: async (_req, file) => {
+		const originalName = file.originalname.replace(/\s+/g, '_');
 
-const storage = multer.diskStorage({
-	destination: (_req, _file, cb) => cb(null, uploadPath),
-	filename: (_req, file, cb) => {
-		const safeName = file.originalname.replace(/\s+/g, '_');
-		const uniqueSuffix = `${Date.now()}-${uuidv4()}`;
-		cb(null, `${uniqueSuffix}-${safeName}`);
+		return {
+			folder: 'profile_photos',
+			publid_id: `${uuidV4()}-${originalName.split('.')[0]}`,
+			format: file.mimetype.split('/')[1],
+			resource_type: 'image'
+		};
 	}
 });
 
@@ -23,10 +22,9 @@ export const upload = multer({
 	storage,
 	limits: { fileSize: ONE_MEGABYTE },
 	fileFilter: (_req, file, cb) => {
-		const ext = path.extname(file.originalname).toLowerCase();
-		const allowedExtensions = ['.png', '.jpg', '.jpeg'];
+		const allowedExtensions = ['image/png', 'image/jpg', 'image/jpeg'];
 
-		if (!allowedExtensions.includes(ext)) {
+		if (!allowedExtensions.includes(file.mimetype)) {
 			return cb(new Error('Only .png, .jpg and .jpeg formats are allowed'));
 		}
 
